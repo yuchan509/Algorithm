69cbce822f610095e6a9c088feb4c44d56222bda
Yuchan <75283773+yuchan509@users.noreply.github.com>
2022-06-21 09:45:50 +0900

    구간 나누기2
    구간 나누기2

--- Binary Search/Solution/구간 나누기2/구간 나누기2.py
+++ Binary Search/Solution/구간 나누기2/구간 나누기2.py
@@ -1,23 +1,43 @@
-def solution(n, times):
-    left, right = min(times), n * max(times)
+import sys
 
-    while left <= right:
+input = sys.stdin.readline
 
-        res = 0
-        mid = (left + right) // 2     
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
 
-        for i in times:
-            res += mid // i 
+def division(mid):
 
-        if res >= n:
-            right = mid - 1
-        else:
-            left = mid + 1
+    m_cnt = 1
+    mx = mn = arr[0]
+    for v in arr:
+        mx = max(mx, v)
+        mn = min(mn, v)
 
-    return left
+        if mid < (mx - mn):
+            m_cnt += 1
+            mx = mn = v
+            
+    
+    return m_cnt
+
+ans = 10000000
+left, right = 0, max(arr)
+while left <= right:
+    mid = (left + right) // 2
+    
+    if division(mid) <= m:
+        right = mid - 1
+        ans = mid if mid < ans else ans
+
+    else:
+        left = mid + 1
+        
+print(ans)
 
 
 # Run.
-n = 6
-times = [7, 10]
-solution(n, times)
+'''
+output : 5
+8 3
+1 5 4 6 2 1 3 7
+'''
