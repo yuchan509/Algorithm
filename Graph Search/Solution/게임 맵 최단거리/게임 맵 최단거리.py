b416906dbb6ca510a25bc1ee37ac7a10689a0353
Yuchan <75283773+yuchan509@users.noreply.github.com>
2022-07-22 14:04:40 +0900

    게임 맵 최단거리

--- Graph Search/Solution/게임 맵 최단거리/게임 맵 최단거리.py
+++ Graph Search/Solution/게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,33 +1,30 @@
 from collections import deque
 
 def solution(maps):
-
-  n, m = len(maps), len(maps[0])
-  dx = [0, 0, -1, 1]
-  dy = [1, -1, 0, 0]
-
-  q = deque()
-  q.append([0, 0, 1])
-  visited = [[False] * m for _ in range(n)]
-  visited[0][0] = True
-
-  while q:
-   x, y, cnt = q.popleft()
-   
-   if x == n - 1 and y == m - 1: return cnt
-
-   for idx in range(4):
-     nx = x + dx[idx]
-     ny = y + dy[idx]
-
-     if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == False:
-       if maps[nx][ny] != 0:
-         visited[nx][ny] = True
-         q.append([nx, ny, cnt + 1])
-
-  if visited[n-1][m-1] == False : return -1
-
-  return cnt
+    
+    n, m = len(maps), len(maps[0])
+    
+    d = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    q = deque()
+    q.append([0, 0])
+    
+    visited = [[0] * m for _ in range(n)]
+    
+    while q:
+        x, y = q.popleft()
+        
+        if x == n - 1 and y == m - 1:
+            return visited[-1][-1] + 1
+        
+        for dx, dy in d:
+            nx = x + dx
+            ny = y + dy
+            
+            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
+                if maps[nx][ny] != 0:
+                    visited[nx][ny] = visited[x][y] + 1
+                    q.append([nx, ny])
+    return -1
 
 
 # Run.(1 : 가능, 0 : 불가능)
