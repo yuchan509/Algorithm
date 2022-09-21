b3da3f2fa29b6e59ec1adb4274d1356c58f2ae13
Yuchan <75283773+yuchan509@users.noreply.github.com>
2022-09-21 17:16:46 +0900

    알파벳

---
+++ Graph Search/Solution/알파벳/알파벳.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque, defaultdict
+
+n, k, m = map(int, input().split())
+graph = defaultdict(list)
+
+for h in range(1, m + 1):
+    arr = list(map(int, sys.stdin.readline().split()))
+    for s in arr:
+        graph[s].append(n + h)
+        graph[n + h].append(s)
+
+def bfs():
+
+    v = set([1])
+    q = deque([(1, 0)])
+
+    while q:
+        x, cnt = q.popleft()
+
+        if x == n:
+            ans = (cnt // 2) + 1
+            return ans
+
+        for node in graph[x]:
+            if node not in v:
+                v.add(node)
+                q.append([node, cnt + 1])
+    return -1
+
+print(bfs())
+
+
+'''
+output : 6
+3 6
+HFDFFB
+AJHGDH
+DGAGEH
+'''
