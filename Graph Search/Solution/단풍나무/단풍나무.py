ff6f0b0f50cd72f4b03d7983fc718a5da83c2196
Yuchan <75283773+yuchan509@users.noreply.github.com>
2022-11-29 23:28:00 +0900

    단풍나무

--- Graph Search/Solution/단풍나무/단풍나무.py
+++ Graph Search/Solution/단풍나무/단풍나무.py
@@ -1,3 +1,4 @@
+
 import sys
 from collections import deque
 input = sys.stdin.readline
@@ -7,37 +8,37 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 d = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
 def bfs():
-	
-	res = []
-	q = deque()
-	for i in range(n):
-		for j in range(n):
-			if arr[i][j] != 0:
-				q.append([i, j])
 
-	while q:
-		x, y = q.popleft()
+    res = []
+    q = deque()
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] != 0:
+                q.append([i, j])
 
-		cnt = 0
-		for dx, dy in d:
-			nx = x + dx
-			ny = y + dy
-			
-			if 0 <= nx < n and 0 <= ny < n:
-				if arr[nx][ny] == 0:
-					cnt += 1
-					
-		res.append([x, y, max(0, arr[x][y] - cnt)])
-		
-	for r, c, v in res:
-		arr[r][c] = v
-		
-	return 1
+    while q:
+        x, y = q.popleft()
+
+        cnt = 0
+        for dx, dy in d:
+            nx = x + dx
+            ny = y + dy
+
+            if 0 <= nx < n and 0 <= ny < n:
+                if arr[nx][ny] == 0:
+                    cnt += 1
+
+        res.append([x, y, max(0, arr[x][y] - cnt)])
+
+    for r, c, v in res:
+        arr[r][c] = v
+
+    return 1
 
 ans = 0
 while max(map(max, arr)) != 0:
-	ans += bfs()
-	
+    ans += bfs()
+
 print(ans)
 
 
