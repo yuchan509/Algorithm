49f21d9c06fd5f49b72303185f0ffec60ceb8c2d
Yuchan <75283773+yuchan509@users.noreply.github.com>
2022-12-20 17:06:02 +0900

    폴더 폰 자판

---
+++ Implementation/Solution/폴더 폰 자판/폴더 폰 자판.py
@@ -0,0 +1,32 @@
+import sys
+sys.stdin.readline
+
+n = int(input())
+s = list(map(int, input()))
+
+phone = ['1.,?!', '2ABC', '3DEF',
+        '4GHI', '5JKL', '6MNO',
+        '7PQRS', '8TUV', '9WXYZ']
+
+ans = ''
+cnt = 0
+for i in range(n - 1):
+	if s[i] == s[i + 1]:
+		cnt += 1
+	
+	else:
+		p = phone[s[i] - 1]
+		ans += p[cnt % len(p)]
+		cnt = 0
+	
+p = phone[s[-1] - 1]
+ans += p[cnt % len(p)]
+
+print(ans)
+
+
+'''
+output : .
+2
+11
+'''
