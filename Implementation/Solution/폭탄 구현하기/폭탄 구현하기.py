b0565471b496822b67e726d72bf707675c33d741
Yuchan <75283773+yuchan509@users.noreply.github.com>
2022-12-20 14:43:56 +0900

    폭탄 구현하기

---
+++ Implementation/Solution/폭탄 구현하기/폭탄 구현하기.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+d = [[1, 0],[0, 1],[-1, 0],[0, -1]]
+arr = [[0] * n for _ in range(n)]
+
+
+for _ in range(k):
+	a, b = map(int, input().split())
+	a -= 1
+	b -= 1
+	arr[a][b] += 1
+	
+	for da, db in d:
+		na = a + da
+		nb = b + db
+		
+		if 0 <= na < n and 0 <= nb < n:
+			arr[na][nb] += 1
+			
+ans = sum(sum(arr, []))
+
+print(ans)
+
+
+'''
+output : 9
+3 3
+3 3
+3 3
+1 1
+'''
