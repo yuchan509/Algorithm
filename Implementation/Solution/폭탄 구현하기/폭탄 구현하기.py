231b13ff20535fb8c4f9635cbeffe23b566d24ec
Yuchan <75283773+yuchan509@users.noreply.github.com>
2022-12-20 14:45:15 +0900

    폭탄 구현하기

--- Implementation/Solution/폭탄 구현하기/폭탄 구현하기.py
+++ Implementation/Solution/폭탄 구현하기/폭탄 구현하기.py
@@ -6,20 +6,19 @@ n, k = map(int, input().split())
 d = [[1, 0],[0, 1],[-1, 0],[0, -1]]
 arr = [[0] * n for _ in range(n)]
 
-
 for _ in range(k):
-	a, b = map(int, input().split())
-	a -= 1
-	b -= 1
-	arr[a][b] += 1
-	
-	for da, db in d:
-		na = a + da
-		nb = b + db
-		
-		if 0 <= na < n and 0 <= nb < n:
-			arr[na][nb] += 1
-			
+    a, b = map(int, input().split())
+    a -= 1
+    b -= 1
+    arr[a][b] += 1
+    
+    for da, db in d:
+        na = a + da
+        nb = b + db
+        
+        if 0 <= na < n and 0 <= nb < n:
+            arr[na][nb] += 1
+            
 ans = sum(sum(arr, []))
 
 print(ans)
