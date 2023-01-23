cb988b1562358045de386a1ec9d9e7d662b64252
Yuchan <75283773+yuchan509@users.noreply.github.com>
2023-01-24 05:55:33 +0900

    회전 초밥

---
+++ Two Pointers/Solution/회전 초밥/회전 초밥.py
@@ -0,0 +1,36 @@
+import sys
+
+input = sys.stdin.readline
+
+n, d, k, c = map(int, input().split())
+
+belt = [int(input()) for _ in range(n)]
+
+ans = 0
+length = len(belt)
+
+for i in range(length):
+    if k + i > length:
+        window = belt[i:length] + belt[:(k + i) % length]
+
+    else:
+        window = belt[i:k + i]
+
+    ans = max(ans, len(set(window + [c])))
+
+print(ans)
+
+
+# Run.
+'''
+output : 5
+8 30 4 30
+7
+9
+7
+30
+2
+7
+9
+25
+'''
