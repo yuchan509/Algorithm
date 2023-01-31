ea92d2cc6d2046d6e660d00b66ade09f3a2bf3cf
Yuchan <75283773+yuchan509@users.noreply.github.com>
2023-02-01 01:46:55 +0900

    Tree

--- Tree/Solution/구간합 구하기/구간합 구하기.py
+++ Tree/Solution/구간합 구하기/구간합 구하기.py
@@ -1,73 +1,93 @@
 import sys
+from collections import deque
+
 input = sys.stdin.readline
 
-def init(node, start, end):
-    if start == end:
-        tree[node] = maps[start]
-        return tree[node]
+t = int(input())
 
-    mid = (start + end) // 2
-    left = node * 2
-    right = (node * 2) + 1
-    tree[node] = init(left, start, mid) + init(right, mid + 1, end)
-
-    return tree[node]
-
-def subSum(node, start, end, left, right):
-    if left > end or right < start:
-        return 0
-
-    if left <= start and end <= right:
-        return tree[node]
-
-    mid = (start + end) // 2
-    l = subSum(node * 2, start, mid, left, right)
-    r = subSum((node * 2) + 1, mid + 1, end, left, right)
-    cumsum = l + r
-
-    return cumsum
-
-def update(node, start, end, index, diff):
-    if index < start or index > end: return
-
-    tree[node] += diff
-    mid = (start + end) // 2
-
-    if start != end:
-        update(node * 2, start, mid, index, diff)
-        update((node * 2) + 1, mid + 1, end, index, diff)
+d = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
 
-n, m, k = map(int, input().split())
-maps = [int(input()) for _ in range(n)]
-tree = [0] * (n * 4)
+def bfs(x, y):
+    q = deque([[x, y]])
+    v = [[-1] * (w + 2) for _ in range(h + 2)]
+    v[x][y] = 0
 
-init(1, 0, n - 1)
-for _ in range(m + k):
-    a, b, c = map(int, input().split())
+    while q:
+        x, y = q.popleft()
 
-    if a == 1:
-        diff = c - maps[b - 1]
-        maps[b - 1] = c
-        update(1, 0, n - 1, b - 1, diff)
-    else:
-        print(subSum(1, 0, n - 1, b - 1, c - 1))
+        for dx, dy in d:
+            nx = x + dx
+            ny = y + dy
+
+            if 0 <= nx < h + 2 and 0 <= ny < w + 2 and v[nx][ny] == -1:
+                if maps[nx][ny] == '#':
+                    v[nx][ny] = v[x][y] + 1
+                    q.append([nx, ny])
+
+                elif maps[nx][ny] in '$.':
+                    v[nx][ny] = v[x][y]
+                    q.appendleft([nx, ny])
+    return v
+
+
+for _ in range(t):
+    h, w = map(int, input().split())
+
+    maps = [['.'] * (w + 2) for x in range(h + 2)]
+    for i in range(h):
+        maps[i + 1][1:-1] = list(input().strip())
+
+    prisoner = [(i, j) for j in range(w + 2)
+                for i in range(h + 2) if maps[i][j] == '$']
+
+    x1, y1 = prisoner[0]
+    x2, y2 = prisoner[1]
+
+    map1 = bfs(0, 0)
+    map2 = bfs(x1, y1)
+    map3 = bfs(x2, y2)
+
+    ans = sys.maxsize
+    for i in range(h + 2):
+        for j in range(w + 2):
+            if maps[i][j] in '$*': continue
+
+            if -1 not in [map1[i][j], map2[i][j], map3[i][j]]:
+
+                cumsum = map1[i][j] + map2[i][j] + map3[i][j]
+                if maps[i][j] == '#':
+                    cumsum -= 2
+
+                ans = min(ans, cumsum)
+    print(ans)
 
 
 # Run.
 '''
-output
-17
-12
+output : 4 0 9
 
-5 2 2
-1
-2
 3
-4
-5
-1 3 6
-2 2 5
-1 5 2
-2 3 5
+5 9
+****#****
+*..#.#..*
+****.****
+*$#.#.#$*
+*********
+5 11
+*#*********
+*$*...*...*
+*$*.*.*.*.*
+*...*...*.*
+*********.*
+9 9
+*#**#**#*
+*#**#**#*
+*#**#**#*
+*#**.**#*
+*#*#.#*#*
+*$##*##$*
+*#*****#*
+*.#.#.#.*
+*********
 '''
